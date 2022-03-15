f76606c092f5136ac9ca72fc912cbe68acdb8ccd
hernandez-crypto <hernandezjulio58@ymail.com>
2022-03-15 16:56:05 -0500

    playing around w/ puzzle4.cpp

--- cpp-heapPuzzles/puzzle4.cpp
+++ cpp-heapPuzzles/puzzle4.cpp
@@ -14,9 +14,14 @@ int main() {
   int *x;
   int size = 3;
   x = new int[size];
+  cout << x << '\n';
+  cout << *x << '\n';
+  cout << &x << '\n';
 
   for (int i = 0; i < size; i++) {
+    cout << x[i] << '\n';
     x[i] = i + 3;
+    cout << x[i] << '\n';
   }
 
   delete[] x;
